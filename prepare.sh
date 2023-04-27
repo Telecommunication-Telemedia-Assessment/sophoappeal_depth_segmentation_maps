37a4a65c1a7914e69b8bb2aa8015c24dd8f7d16d
Steve Göring <steve.goering@tu-ilmenau.de>
2023-04-27 14:27:33 +0200

    changes

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget "https://ftp.tu-ilmenau.de/hpc-private/ei/avt/sophoappeal/depth_segmentation_maps_data.zip"
+wget "https://resdata.tu-ilmenau.de/public/ei/avt/sophoappeal/depth_segmentation_maps_data.zip"
 # extract
 unzip depth_segmentation_maps_data.zip
 
